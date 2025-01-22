a3601374b9bd439aa1b5d7e11ac4a4dbccc681a8
Ange230700 <kouakouangeericstephane@gmail.com>
2025-01-22 10:43:11 +0100

    fix: 🐛  allowed some origin to bypass CORS policy

--- server/application/utils/allowedOrigins.cjs
+++ server/application/utils/allowedOrigins.cjs
@@ -1,6 +1,9 @@
 // server\application\utils\allowedOrigins.cjs
 
 // eslint-disable-next-line no-undef
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "https://05-recursivity-client.vercel.app",
+];
 
 module.exports = allowedOrigins;
